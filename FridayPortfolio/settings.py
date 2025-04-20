21bd00de796f57e242f3f16163ba8b7c55713f25
John Rey Mart <johnreymartviernes@gmail.com>
2025-04-20 11:46:45 +0800

    Updated settings.py

--- FridayPortfolio/settings.py
+++ FridayPortfolio/settings.py
@@ -14,7 +14,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'secret.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env.bool('DJANGO_DEBUG', default=True)  # Reading DEBUG from env or default to True
 
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'my-portfolio-dpyu.onrender.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'my-portfolio-16pg.onrender.com'])
 
 # Database configuration
 DATABASES = {
