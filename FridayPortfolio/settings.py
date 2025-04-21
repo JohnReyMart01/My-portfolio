8021312c25e78305bfe8c843e0a49cf8fd46c86e
John Rey Mart <johnreymartviernes@gmail.com>
2025-04-21 17:26:51 +0800

    Added WhiteNoise and fixed static settings

--- FridayPortfolio/settings.py
+++ FridayPortfolio/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +71,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "main" / "static",
 ]
 
+# Use WhiteNoise for serving static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
